aedf37b1210844f3affc144fe586cf61544d14a7
Melvin <melvin.cheng@live.com>
2017-04-18 12:54:09 -0400

    Working

--- char_count.c
+++ char_count.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[]){
 	replace_table_t *table;
 	FILE *fp_input, *fp_replace;
 	char string[256];
-	char *final_string, *temp_char, *token, *temp_final_string;
+	char **final_string, *temp_char, *token, *temp_final_string;
 	char *prev_string = NULL;
 	char *new_string = NULL;
 	int total_length = 0;
@@ -75,70 +75,91 @@ int main(int argc, char *argv[]){
 	}
 
 	// reads the input file and puts the entire file into a single string
-	while(fgets(string, 256, fp_input) != NULL){
-		total_length += strlen(string);
-		temp_char = realloc(final_string, sizeof(char) * total_length);
-		if(!temp_char){
-			fprintf(stderr, "Unable to realloc\n");
-			exit(-1);
-		}
-		final_string = temp_char;
-		strcat(final_string, string);
+	// while(fgets(string, 256, fp_input) != NULL){
+	// 	total_length += strlen(string);
+	// 	temp_char = realloc(final_string, sizeof(char) * total_length);
+	// 	if(!temp_char){
+	// 		fprintf(stderr, "Unable to realloc\n");
+	// 		exit(-1);
+	// 	}
+	// 	final_string = temp_char;
+	// 	strcat(final_string, string);
+	// }
+
+	char temp_string[256];
+	char **char_arr = NULL;
+	int total = 0;
+	while(fscanf(fp_input, "%s", temp_string) > 0){
+		// printf("%s\n", temp_string);
+		// total_length += strlen(temp_string);
+		final_string = realloc(final_string, sizeof(char*) * ++total);
+		final_string[total - 1] = malloc(256);
+		strcpy(final_string[total - 1], temp_string);
+		// strcat(final_string, temp_string);
+		total_length++;
 	}
 
+	// for(int i = 0; i < length; i++){
+	// 	printf("%s\n", final_string[i]);
+	// }
+	// printf("done\n");
 	// copies the string so that the final string is not affected
 	// temp_final_string = malloc(sizeof(char) * strlen(final_string));
 	// strcpy(temp_final_string, final_string);
 
 	// seperates words into another array
-	int final_string_length_counter = 0;
-	token = strtok(final_string, " \n\r\t");
-	while(token){
-		final_string_length_counter += strlen(token);
+	// int final_string_length_counter = 0;
+	// token = strtok(final_string, " \n\r\t");
+	// while(token){
+	// 	final_string_length_counter += strlen(token);
 		
-		total_words++;
-		string_array = realloc(string_array, sizeof(char*) * total_words);
-		string_array[total_words - 1] = malloc(sizeof(char) * strlen(token));
+	// 	total_words++;
+	// 	printf("%s\n", token);
+	// 	printf("%d\n", total_words);
+	// 	printf("%d\n", strlen(token));
+	// 	string_array = realloc(string_array, sizeof(char*) * total_words);
+	// 	string_array[total_words - 1] = (char*)malloc(sizeof(char) * strlen(token));
 
-		strcpy(string_array[total_words - 1], token);
+	// 	strcpy(string_array[total_words - 1], token);
 
-		while(final_string[final_string_length_counter] == ' ' ||
-			final_string[final_string_length_counter] == '\r' ||
-			final_string[final_string_length_counter] == '\n' ||
-			final_string[final_string_length_counter] == '\t' ||
-			final_string[final_string_length_counter] == NULL){
-			if(final_string[final_string_length_counter] == ' ' ||
-				final_string[final_string_length_counter] == NULL){
-				total_words++;
-				string_array = realloc(string_array, sizeof(char*) * total_words);
-				string_array[total_words - 1] = malloc(sizeof(char) * 1);
-				strcpy(string_array[total_words - 1], " ");
-				final_string_length_counter++;
-			}else if(final_string[final_string_length_counter] == '\n'){
-				total_words++;
-				string_array = realloc(string_array, sizeof(char*) * total_words);
-				string_array[total_words - 1] = malloc(sizeof(char) * 1);
-				strcpy(string_array[total_words - 1], "\n");
-				final_string_length_counter++;
-			}else if(final_string[final_string_length_counter] == '\t'){
-				total_words++;
-				string_array = realloc(string_array, sizeof(char*) * total_words);
-				string_array[total_words - 1] = malloc(sizeof(char) * 1);
-				strcpy(string_array[total_words - 1], "\t");
-				final_string_length_counter++;
-			}else if(final_string[final_string_length_counter] == '\r'){
-				final_string_length_counter++;
-			}
-			// sleep(1);
-			// printf("%d\r", (int)final_string[final_string_length_counter]);
-		}
-		token = strtok(NULL, " \n\r\t");
-	}
+	// 	while(final_string[final_string_length_counter] == ' ' ||
+	// 		final_string[final_string_length_counter] == '\r' ||
+	// 		final_string[final_string_length_counter] == '\n' ||
+	// 		final_string[final_string_length_counter] == '\t' ||
+	// 		final_string[final_string_length_counter] == NULL){
+	// 		if(final_string[final_string_length_counter] == ' ' ||
+	// 			final_string[final_string_length_counter] == NULL){
+	// 			total_words++;
+	// 			string_array = realloc(string_array, sizeof(char*) * total_words);
+	// 			string_array[total_words - 1] = malloc(sizeof(char) * 1);
+	// 			strcpy(string_array[total_words - 1], " ");
+	// 			final_string_length_counter++;
+	// 		}else if(final_string[final_string_length_counter] == '\n'){
+	// 			total_words++;
+	// 			string_array = realloc(string_array, sizeof(char*) * total_words);
+	// 			string_array[total_words - 1] = malloc(sizeof(char) * 1);
+	// 			strcpy(string_array[total_words - 1], "\n");
+	// 			final_string_length_counter++;
+	// 		}else if(final_string[final_string_length_counter] == '\t'){
+	// 			total_words++;
+	// 			string_array = realloc(string_array, sizeof(char*) * total_words);
+	// 			string_array[total_words - 1] = malloc(sizeof(char) * 1);
+	// 			strcpy(string_array[total_words - 1], "\t");
+	// 			final_string_length_counter++;
+	// 		}else if(final_string[final_string_length_counter] == '\r'){
+	// 			final_string_length_counter++;
+	// 		}
+	// 		// sleep(1);
+	// 		// printf("%d\r", (int)final_string[final_string_length_counter]);
+	// 	}
+	// 	token = strtok(NULL, " \n\r\t");
+	// }
 
-	total_words++;
-	string_array = realloc(string_array, sizeof(char*) * total_words);
-	string_array[total_words - 1] = malloc(sizeof(char) * 1);
-	strcpy(string_array[total_words - 1], "\n");
+	// total_words++;
+	// string_array = realloc(string_array, sizeof(char*) * total_words);
+	// string_array[total_words - 1] = malloc(sizeof(char) * 1);
+	// strcpy(string_array[total_words - 1], "\n");
+	// printf("done\n");
 
 	// for(int i = 0; i < table_size; i++){
 	// 	printf("%s %s\n", table[i].prev_string, table[i].new_string);
@@ -148,28 +169,34 @@ int main(int argc, char *argv[]){
 	// 	printf("%s\n", string_array[i]);
 	// }
 
-	printf("Before replacements\n");
-	printf("-----\n");
-	for(int i = 0; i < total_words; i++){
-		printf("%s", string_array[i]);
-	}
-	printf("-----\n\n");
+	// printf("Before replacements\n");
+	// printf("-----\n");
+	// for(int i = 0; i < total_words; i++){
+	// 	printf("%s", string_array[i]);
+	// }
+	// printf("-----\n\n");
 
 
 	// count characters
-	#pragma omp parallel
+	#pragma omp parallel num_threads(100)
 	{
 		#pragma omp for
-		for(int i = 0; i < total_words; i++){
-			for(int j = 0; j < strlen(string_array[i]); j++){
-				char_search(string_array[i][j]);
+		// #pragma omp parallel for num_threads(1)
+		// for(int i = 0; i < total_length; i++){
+		// 	for(int j = 0; j < strlen(final_string[i]); j++){
+		// 		char_search(final_string[i][j]);
+		// 	}
+		// }
+		for(int i = 0; i < total_length; i++){
+			for(int j = 0; j < strlen(final_string[i]); j++){
+				char_search(final_string[i][j]);
 			}
-			word_search(string_array[i]);
+			word_search(final_string[i]);
 		}
 
 		#pragma omp for
-		for(int i = 0; i < total_words; i++){
-			word_replace(string_array[i], table);
+		for(int i = 0; i < total_length; i++){
+			word_replace(final_string[i], table);
 		}
 	}
 
@@ -191,11 +218,11 @@ int main(int argc, char *argv[]){
 	printf("-----\n\n");
 
 
-	printf("After replacements\n");
-	printf("-----\n");
-	for(int i = 0; i < total_words; i++){
-		printf("%s", string_array[i]);
-	}
+	// printf("After replacements\n");
+	// printf("-----\n");
+	// for(int i = 0; i < total_words; i++){
+	// 	printf("%s", string_array[i]);
+	// }
 
 	fclose(fp_input);
 }
